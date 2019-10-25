8738927c5d791a61b2b0b7308754321b7ad11f3e
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-10-25 11:28:17 -0400

    took out page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,11 +24,11 @@ const routes: Routes = [
       path: '',
       redirectTo: '/home',
       pathMatch: 'full'
-    },
-    {
-      path: '**',
-      component: PageNotFoundComponent
     }
+    // {
+    //   path: '**',
+    //   component: PageNotFoundComponent
+    // }
 ];
 
 @NgModule({
